a36d062a79e77cc97a6eb5b28fd369252e4e9e22
Nguyễn Kim Huy <nkhuy05@gmail.com>
2024-12-09 06:07:04 +0700

    Update CrawlTask.java

--- twitter-influencer-ranking/src/main/java/CrawlData/CrawlTask.java
+++ twitter-influencer-ranking/src/main/java/CrawlData/CrawlTask.java
@@ -3,15 +3,17 @@ package CrawlData;
 public class CrawlTask implements Runnable{
     private String filePath1;
     private String filePath2;
+    private ExcelFileWriter excelFileWriter;
 
-    public CrawlTask(String filePath1, String filePath2) {
+    public CrawlTask(String filePath1, String filePath2, ExcelFileWriter excelFileWriter) {
         this.filePath1 = filePath1;
         this.filePath2 = filePath2;
+        this.excelFileWriter = excelFileWriter;
     }
 
     @Override
     public void run() {
             MainCrawl1 mainCrawl = new MainCrawl1();
-            mainCrawl.ControlMainCrawl(filePath1, filePath2);
+            mainCrawl.ControlMainCrawl(filePath1, filePath2, excelFileWriter);
     }
 }
