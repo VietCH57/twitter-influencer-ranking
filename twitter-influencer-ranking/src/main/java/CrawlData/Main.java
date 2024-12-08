3f8b6c973429137269d069ecd9fe5ad6af0899b9
Nguyễn Kim Huy <nkhuy05@gmail.com>
2024-12-09 06:08:22 +0700

    Update Main.java

--- twitter-influencer-ranking/src/main/java/CrawlData/Main.java
+++ twitter-influencer-ranking/src/main/java/CrawlData/Main.java
@@ -6,13 +6,13 @@ import java.util.concurrent.Executors;
 public class Main {
     public static void main(String[] args) {
 
-        ExecutorService executor = Executors.newFixedThreadPool(2);
-
-        String[] filePaths1 = {"D:\\Documents D\\userName1.txt"};
-        String filePaths2 = "D:\\Documents D\\CrawlResult.xlsx";
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        String[] filePaths1 = {"C:\\Users\\Administrator\\Documents\\userName1.txt", "C:\\Users\\Administrator\\Documents\\userName2.txt", "C:\\Users\\Administrator\\Documents\\userName3.txt", "C:\\Users\\Administrator\\Documents\\userName4.txt", "C:\\Users\\Administrator\\Documents\\userName5.txt", "C:\\Users\\Administrator\\Documents\\userName6.txt", "C:\\Users\\Administrator\\Documents\\userName7.txt", "C:\\Users\\Administrator\\Documents\\userName8.txt", "C:\\Users\\Administrator\\Documents\\userName9.txt", "C:\\Users\\Administrator\\Documents\\userName10.txt"};
+        String filePaths2 = "C:\\Users\\Administrator\\Documents\\CrawlResult.xlsx";
+        ExcelFileWriter ew = new ExcelFileWriter(filePaths2);
         for (int i = 0; i < filePaths1.length; i++) {
             try {
-                CrawlTask task = new CrawlTask(filePaths1[i], filePaths2);
+                CrawlTask task = new CrawlTask(filePaths1[i], filePaths2, ew);
                 executor.execute(task);
                 Thread.sleep(35000);
             } catch (Exception e) {
